4c70df6590495aa0e737adc67bc5a0ebb773bb03
ashchesnovich <as@visioners.tech>
2025-05-04 14:12:27 +0300

    test done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
+import Header from './components/Header/Header';
 
 
 function App() {
-    const {onToggleButton, tg} = useTelegram()
-  
-    useEffect(()=>{
-      tg.ready();
-    },[tg])
+  const {onToggleButton, tg} = useTelegram()
+
+  useEffect(()=>{
+    tg.ready();
+  },[tg])
 
 
   return (
     <div className="App">
+      <Header />
       <button onClick={onToggleButton}>toggle</button>
     </div>
   );
