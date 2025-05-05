aa57cff164efb6be2380c8f38176b1c6cf15c84f
ashchesnovich <as@visioners.tech>
2025-05-05 13:14:20 +0300

    hot fix

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./ProductList.css";
-import { useState } from "react";
+import { useState, useCallback, useEffect } from "react";
 import ProductItem from "../ProductItem/ProductItem";
 import { useTelegram } from "../../hooks/useTelegram";
 
